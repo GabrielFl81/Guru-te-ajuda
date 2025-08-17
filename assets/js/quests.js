c0105f9859ba37745579b4e45b181bc29f403958
GabrielFl81 <gabriel.lopes.silva24@escola.pr.gov.br>
2025-08-17 20:55:57 -0300

    Add files via upload

---
+++ assets/js/quests.js
@@ -0,0 +1,53 @@
+criaCartao(
+    'Música',
+    'O que é um semitom?',
+    'É a menor distância entre duas notas'
+)
+
+criaCartao(
+    'Música',
+    'Como é o nome do simbolo que simboliza uma nota por 4 tempos?',
+    'A semibreve, ela é um simbolo ritmico que marca uma nota por 4 tempos'
+)
+
+criaCartao(
+    'Música',
+    'O que é uma clave?',
+    'São simbolos que nomeam as alturas das notas na palta como clave de Fá'
+)
+
+criaCartao(
+    'Música',
+    'O que é um dobrobemol?',
+    'É a alteração da altura de uma nota natural de dois semitons abaixo'
+)
+
+criaCartao(
+    'Música',
+    'O que é um acidente na música?',
+    'É a alteração de altura de uma nota natural'
+)
+
+criaCartao(
+    'Música',
+    'O que é uma escala tonal?',
+    'É a uma escala de notas que segue um padrão de tons e semitons(pode ser maior ou menor)'
+)
+
+criaCartao(
+    'Música',
+    'Qual é a escala de Dó maior natural?',
+    'A escala de Dó maior natural é:<br>C-D-E-F-G-A-B'
+)
+
+criaCartao(
+    'Música',
+    'O que é um intervalo musical?',
+    'É a distancia de notas medidas em tons e semitons como: 1°, 3° e 5°'
+)
+
+criaCartao(
+    'Música geral',
+    'Quem foi a criadora do rock?',
+    'A criadora do Rock era Sister Rosetta Tharpe'
+)
